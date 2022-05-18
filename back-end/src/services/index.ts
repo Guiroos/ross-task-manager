a5c780513c8b42bdc2b7bfd40e57766754d9a06d
guiroos <guilherme.roosr@gmail.com>
2022-05-18 20:59:34 -0300

    refactor: changed protected to public

--- back-end/src/services/index.ts
+++ back-end/src/services/index.ts
@@ -2,7 +2,7 @@ import { Model } from '../interfaces/ModelInterface';
 import { ServiceError } from '../interfaces/ServiceErrorInterface';
 
 export default abstract class Service<T> {
-  constructor(protected model: Model<T>) { }
+  constructor(public model: Model<T>) { }
 
   public async create(data: T): Promise<T | null | ServiceError> {
     return this.model.create(data);
