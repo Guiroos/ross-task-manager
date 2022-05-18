ed0c607a2a36a233e828a54050d814e8c96260af
guiroos <guilherme.roosr@gmail.com>
2022-05-18 19:19:00 -0300

    feat: created abstract Service Class

---
+++ back-end/src/services/index.ts
@@ -0,0 +1,10 @@
+import { Model } from '../interfaces/ModelInterface';
+import { ServiceError } from '../interfaces/ServiceErrorInterface';
+
+export default abstract class Service<T> {
+  constructor(protected model: Model<T>) { }
+
+  public async create(data: T): Promise<T | null | ServiceError> {
+    return this.model.create(data);
+  }
+}
