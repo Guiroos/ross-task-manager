bf8ff0df1adb4ab2a57d4c29af21ba1193ff97a4
guiroos <guilherme.roosr@gmail.com>
2022-05-19 11:06:12 -0300

    faet: add readOne function to abstract service

--- back-end/src/services/index.ts
+++ back-end/src/services/index.ts
@@ -11,4 +11,8 @@ export default abstract class Service<T> {
   public async read(): Promise<T[] | ServiceError> {
     return this.model.read();
   }
+
+  public async readOne(id: string): Promise<T | null | ServiceError> {
+    return this.model.readOne(id);
+  }
 }
