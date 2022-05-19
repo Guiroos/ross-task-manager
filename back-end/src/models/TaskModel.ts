6835d29421218e9bce0fd53e983f851a5a4f5c0a
guiroos <guilherme.roosr@gmail.com>
2022-05-19 11:16:02 -0300

    refactor: task model exports and version key

--- back-end/src/models/TaskModel.ts
+++ back-end/src/models/TaskModel.ts
@@ -2,13 +2,13 @@ import { Schema, model as createModel, Document } from 'mongoose';
 import { Task } from '../interfaces/TaskInterface';
 import MongoModel from '.';
 
-interface TaskDocument extends Task, Document { }
+export interface TaskDocument extends Task, Document { }
 
-const TaskSchema = new Schema<TaskDocument>({
+export const TaskSchema = new Schema<TaskDocument>({
   title: String,
   description: String,
   status: String,
-});
+}, { versionKey: false });
 
 export default class TaskModel extends MongoModel<Task> {
   constructor(model = createModel('tasks', TaskSchema)) {
