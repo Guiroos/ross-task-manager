0ca051218ba9e99352c45b513e3f3217229fd857
guiroos <guilherme.roosr@gmail.com>
2022-05-24 00:43:49 -0300

    feat: created scripts and package.json

---
+++ scripts/preStartBE.sh
@@ -0,0 +1,7 @@
+printf "\n>Installing dependencies\n"
+
+cd ./backend
+npm install
+npm run dev
+
+printf "\n>Done\n"
---
+++ scripts/preStartFE.sh
@@ -0,0 +1,7 @@
+printf "\n>Installing dependencies\n"
+
+cd ./frontend
+npm install
+npm start
+
+printf "\n>Done\n"
